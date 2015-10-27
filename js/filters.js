29948cd2943e5b3b1056949938117063506dd842
Ninh <tmhn89@gmail.com>
2015-10-27 16:08:43 +0700

    Update CV & fix portfolio

--- js/filters.js
+++ js/filters.js
@@ -7,3 +7,9 @@ filters.filter('SentenceCase', function()
         return input.substring(0,1).toUpperCase() + input.slice(1);
     };
 });
+// replace || with line break
+filters.filter('Unsafe', function($sce) {
+    return function(input) {
+        return $sce.trustAsHtml(input);
+    };
+});
